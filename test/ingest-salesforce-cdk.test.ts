bae1eb3b76bf545418cb16fb09cc7f9fbd9f4970
Augusto Coelho <augusto.henriques@colab.re>
2025-08-25 16:26:48 -0300

    Initial commit

---
+++ test/ingest-salesforce-cdk.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as IngestSalesforceCdk from '../lib/ingest-salesforce-cdk-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/ingest-salesforce-cdk-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new IngestSalesforceCdk.IngestSalesforceCdkStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
